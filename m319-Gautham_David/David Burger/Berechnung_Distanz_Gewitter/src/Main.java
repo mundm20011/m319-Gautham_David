e62361c56c7dd5c1cb77644d8c18449eb7170657
mundm20011 <burgerdavid6@gmail.com>
2023-09-06 13:24:40 +0200

    Offline arbeiten

--- m319-Gautham_David/David Burger/Berechnung_Distanz_Gewitter/src/Main.java
+++ m319-Gautham_David/David Burger/Berechnung_Distanz_Gewitter/src/Main.java
@@ -1,7 +1,7 @@
 /*Berechnung Distanz Gewitter
 Schreiben Sie eine Klasse GewitterBerechner, der die Entfernung eines
 Gewitters berechnet.
-Die Schallgeschwindigkeit in Luft beträgt etwa 344 m/s (Meter pro Sekunde).
+Die schallSchallgeschwindigkeit in Luft beträgt etwa 344 m/s (Meter pro Sekunde).
 Lassen Sie den Benutzer die entsprechenden Sekunden eingeben.
 Welche Datentypen verwenden wir für die Variablen?
 Wiederholen Sie die Berechnung, wenn der Benutzer dies wünscht (Do ... while) */
@@ -12,7 +12,7 @@ Wiederholen Sie die Berechnung, wenn der Benutzer dies wünscht (Do ... while) *
 // Imports libraries
 
 // Imports Input functions
-//import static lib.Input.*;       // All the functions can be used now!
+import static lib.Input.*;       // All the functions can be used now!
 
 //Every program must be placed in a class ...
 public class Main {
@@ -21,27 +21,30 @@ public class Main {
 		public static void main( String[] args ) {
 
 			//definition variables:
-			double gallons;
-			double litres;
+			double sekunde = inputDouble("bitte geben die sekunden ein");
+			double schallgeschwindigkeit;
+			double distanz;
 
 			//assigns a value:
-			gallons = 0;
+			distanz = 0;
 
 			//calculates results:
-			litres = gallons / 3.7854;
+			schallgeschwindigkeit = distanz / 3.7854;
+			schallgeschwindigkeit = 344;
+			distanz = schallgeschwindigkeit * sekunde;
 
-			while (gallons < 100) {
-				gallons = gallons + 1;
-				if (gallons == 10 || gallons == 20 || gallons == 30 || gallons == 40 || gallons == 50 || gallons == 60 || gallons == 70 || gallons == 80 || gallons == 90) {
-					//System.out.println(gallons + " gallons is " + litres + " litres.");
-					System.out.println(gallons + "/" + litres + "//");
+			while (distanz < 100) {
+				distanz = distanz + 1;
+				if (distanz == 10 || distanz == 20 || distanz == 30 || distanz == 40 || distanz == 50 || distanz == 60 || distanz == 70 || distanz == 80 || distanz == 90) {
+					//System.out.println(distanz + " distanz is " + schallgeschwindigkeit + " schallgeschwindigkeit.");
+					System.out.println(distanz + "/" + schallgeschwindigkeit + "//");
 				} else {
-				//System.out.print(gallons + " gallons is " + litres + " litres.");
-				System.out.print(gallons + "/" + litres + "//");
+				//System.out.print(distanz + " distanz is " + schallgeschwindigkeit + " schallgeschwindigkeit.");
+				System.out.print(distanz + "/" + schallgeschwindigkeit + "//");
 				}
 			}
 
 			//print out using the System libary:
-			//System.out.println(gallons + " gallons is " + litres + " litres.");
+			//System.out.println(distanz + " distanz is " + schallgeschwindigkeit + " schallgeschwindigkeit.");
 		}
 }
--- m319-Gautham_David/David Burger/Celsius-Fahrenheit_Berechner/src/Main.java
+++ m319-Gautham_David/David Burger/Celsius-Fahrenheit_Berechner/src/Main.java
@@ -3,6 +3,7 @@ Schreiben Sie ein Programm, das dem Benutzer erlaubt, entweder die Grad in
 Celcius oder Fahrenheit zu berechnen. Überlegen Sie sich zuerst, welche
 Datentypen Sie verwenden sollten.
 Geben Sie eine Meldung aus, wenn die Temperatur unter 0℃ (Gefrierpunkt), oder 100℃ (Siedepunkt) ist! */
+//273,15=0°
 //fahrenheit = (celsius * 9 / 5 + 32);
 
 // Defines package
@@ -19,16 +20,38 @@ public class Main {
     //our main function which runs the program
 		public static void main( String[] args ) {
 
-			//definition variables:
-			double fahrenheit;
+			//eingabe auswahl:
+			System.out.println("Bitte geben sie ein ob sie Grad->Fahrenheit oder Fahrenheit->Grad erfahren wollen");
+			String auswahl = inputString("//grad/fahrenheit: ");
+			String grad = "grad";
 
 			//assigns a value:
-			double celsius = inputDouble("Geben sie die Temperatur in Grand Celsius ein: ");
+			if (auswahl == grad) {
+			double celsius = inputDouble("Geben sie die Temperatur in Grad Celsius ein: ");
+			double fahrenheit;
+			fahrenheit = (celsius * 9 / 5 + 32);
+			System.out.println(celsius + "//" + fahrenheit);
+				if (celsius >= 100) {
+					System.out.println("Die Temperatur hat der Siedepunkt erreicht");
+				} if (celsius <= 0) {
+					System.out.println("Die Temperatur hat der Gefrierpunkt erreicht");
+				}
+			} else {
+				double fahrenheit = inputDouble("Geben sie die Temperatur in Grad Fahrenheit ein: ");
+				double celsius;
+				celsius = ((fahrenheit - 32) / 5 * 9);
+				System.out.println(fahrenheit + "//" + celsius);
+				if (celsius <= 100) {
+					System.out.println("Die Temperatur hat der Siedepunkt erreicht");
+				} if (celsius >= 0) {
+					System.out.println("Die Temperatur hat der Gefrierpunkt erreicht");
+				}
+			}
 
 			//calculates results:
-			fahrenheit = (celsius * 9 / 5 + 32);
+			//fahrenheit = (celsius * 9 / 5 + 32);
 
-			System.out.println(celsius + "//" + fahrenheit);
+			//System.out.println(celsius + "//" + fahrenheit);
 
 			//print out using the System libary:
 			//System.out.println(celsius + " celsius is " + fahrenheit + " fahrenheit.");
