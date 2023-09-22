b46fcfefce6ee28ff6f64f4f50de94b297ce99e4
mundm20011 <burgerdavid6@gmail.com>
2023-09-22 15:09:57 +0200

    TestKonv erstellt

---
+++ m319-Gautham_David/David Burger/N2-Type_Conversions/Template/src/ch/tbz/TestKonv.java
@@ -0,0 +1,23 @@
+package ch.tbz;
+
+import static ch.tbz.lib.Input.*;       // All the functions can be used now!
+
+public class TestKonv {
+    public static void main(String[] args) {
+        double d;
+        do {
+            d = inputDouble("Geben Sie eine Testzahl ein:");
+
+            float f = (float) d;    // >>> explicit conversion to float
+            int i   =   (int) d;    // >>> explicit conversion to int
+            byte b  =  (byte) d;    // >>> explicit conversion to byte
+            char c  =  (char) d;    // >>> explicit conversion to char
+
+            System.out.println("Double value " + d);
+            System.out.println("Float  value " + f);
+            System.out.println("Int    value " + i);
+            System.out.println("Byte   value " + b);
+            System.out.println("Char   value \'" + c + "\'\n\n");
+        } while( d != 0 );
+    }
+}
