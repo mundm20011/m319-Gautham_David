f9919cbecf0c27d54b3e1cb45880d5e127c00981
mundm20011 <burgerdavid6@gmail.com>
2023-09-29 15:37:57 +0200

    keine ahnung

---
+++ m319-Gautham_David/David Burger/I3/Template/src/ch/tbz/Fibonnaci.java
@@ -0,0 +1,29 @@
+// Defines package
+package ch.tbz;
+
+// Imports libraries
+//import static java.lang.System.*;   // System-IO Library for abbriviation purpose to static functions
+//import static java.lang.Math.*;     // Mathematic Library for abbriviation purpose to static functions
+//import java.util.*;                 // Random is part of this library
+
+// Imports Input functions
+import static ch.tbz.lib.Input.*;       // All the functions can be used now!
+
+//Every program must be placed in a class ...
+public class Fibonnaci {
+
+    // Our main function which runs the program12
+    public static void main(String[] args ) {
+
+        // Title
+        System.out.println("Fibonnaci Zahlen:");
+
+        // Input
+        double zu_erreichender_zahl = inputDouble("Geben Sie die zu erreichender zahl ein: ");
+        double b = inputDouble("Geben Sie die Seite b ein: ");
+
+        // Calculation
+
+
+    }
+}
