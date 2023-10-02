23c09d37b1cfd30de24d8ce300b7aeb365f58523
mundm20011 <burgerdavid6@gmail.com>
2023-10-02 12:22:57 +0200

    Offline Arbeit

--- m319-Gautham_David/David Burger/I3/Template/src/ch/tbz/Fibonnaci.java
+++ m319-Gautham_David/David Burger/I3/Template/src/ch/tbz/Fibonnaci.java
@@ -9,6 +9,10 @@ package ch.tbz;
 // Imports Input functions
 import static ch.tbz.lib.Input.*;       // All the functions can be used now!
 
+/* Die Fibonacci-Folge ist eine unendliche Folge von Zahlen (den Fibonacci-Zahlen),
+bei der sich die jeweils folgende Zahl durch Addition ihrer beiden vorherigen Zahlen ergibt: 0, 1, 1, 2, 3, 5, 8, 13/
+http://www.mat.uc.pt/~mat1164/FibonacciAN.html */
+
 //Every program must be placed in a class ...
 public class Fibonnaci {
 
@@ -19,11 +23,44 @@ public class Fibonnaci {
         System.out.println("Fibonnaci Zahlen:");
 
         // Input
-        double zu_erreichender_zahl = inputDouble("Geben Sie die zu erreichender zahl ein: ");
-        double b = inputDouble("Geben Sie die Seite b ein: ");
+        int start_zahl = inputInt("Geben Sie die Seite b ein: ");
+        int zähler = start_zahl;
+        int wechsel_zahl = 0;
+        int zahl1 = 1;
+        int zahl2 = 0;
+        int zu_erreichender_zahl = inputInt("Geben Sie die zu erreichender zahl ein: ");
+        int ergebnis = 0;
 
         // Calculation
+        do {
 
+            if (ergebnis == start_zahl && start_zahl == 0) {
+                System.out.println(ergebnis);
+            }
+            if (wechsel_zahl == 0) {
+                ergebnis = zahl1 + zahl2;
+                zahl1 = ergebnis;
+                wechsel_zahl = wechsel_zahl + 1;
+                zähler = zähler + 1;
+            }
+            if (ergebnis >= start_zahl) {
+                System.out.println(ergebnis);
+            }
+            if (ergebnis > zu_erreichender_zahl) {
+                break;
+            }
+            else if (wechsel_zahl == 1); {
+                ergebnis = zahl1 + zahl2;
+                zahl2 = ergebnis;
+                wechsel_zahl = wechsel_zahl - 1;
+                zähler = zähler + 1;
+            }
+            if (ergebnis >= start_zahl) {
+                System.out.println(ergebnis);
+            }
+
+        } while (ergebnis < zu_erreichender_zahl);
+//muss noch ein break einbauen damit es nicht das else if ausführt wenn die zahl erreicht wurde
 
     }
 }
